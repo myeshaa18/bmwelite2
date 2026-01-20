3c3c9271788f19a57b8042964f7d2a547bcd1a38
myeshaa18 <164016663+myeshaa18@users.noreply.github.com>
2026-01-20 16:34:19 +0400

    Update auth.js

--- bmw-elite - Copy/js/auth.js
+++ bmw-elite - Copy/js/auth.js
@@ -1,7 +1,7 @@
 // auth.js - Authentication Functionality
 
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = '/api';
 
 // Initialization
 document.addEventListener('DOMContentLoaded', function () {
